0929c2871c77ad8468299dd8e6433c0b1319e7e7
matt.penrice <matthew.penrice@gmail.com>
2014-01-10 18:57:47 +0000

    anotation

--- Generator/SerializableEntityGenerator.php
+++ Generator/SerializableEntityGenerator.php
@@ -111,7 +111,7 @@ use JMS\Serializer\Annotation as JMS;
         $this->staticReflection[$metadata->name]['methods'][] = $methodName;
 
         $replacements = array(
-            '<name>'        => $methodName,
+            '<name>'        => '',
             '<methodName>'  => $methodName,
             '<spaces>'       => sprintf('return "%s";', $metadata->getTableName())
         );
