f060b37dd8744351065f4b9444c635e43be25176
matt.penrice <matthew.penrice@gmail.com>
2014-01-24 13:20:32 +0000

    user object

--- Security/User/BasicUser.php
+++ Security/User/BasicUser.php
@@ -2,23 +2,108 @@
 
 namespace BorderForce\Drt\EntityBundle\Security\User;
 
-use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
-class BasicUser extends User {
+class BasicUser implements AdvancedUserInterface {
   
-  private $name;
-  
-  public function __construct($username, $password, array $roles = array(), $enabled = true, $userNonExpired = true, $credentialsNonExpired = true, $userNonLocked = true) {
-    parent::__construct($username, $password, $roles, $enabled, $userNonExpired, $credentialsNonExpired, $userNonLocked);
-    $this->name = 'theodore "trousered" thunderpockets';
-  }
-  
-  public function getName() {
-    return $this->name;
-  }
-  
-  public function setName($name) {
-    $this->name = $name;
-  }
+    private $username;
+    private $password;
+    private $name;
+    private $enabled;
+    private $accountNonExpired;
+    private $credentialsNonExpired;
+    private $accountNonLocked;
+    private $roles;
+
+    public function __construct($username, $password, $name, array $roles = array(), $enabled = true, $userNonExpired = true, $credentialsNonExpired = true, $userNonLocked = true)
+    {
+        if (empty($username)) {
+            throw new \InvalidArgumentException('The username cannot be empty.');
+        }
+
+        $this->username = $username;
+        $this->password = $password;
+        $this->name     = $name;
+        $this->enabled = $enabled;
+        $this->accountNonExpired = $userNonExpired;
+        $this->credentialsNonExpired = $credentialsNonExpired;
+        $this->accountNonLocked = $userNonLocked;
+        $this->roles = $roles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    public function getName() {
+      return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAccountNonExpired()
+    {
+        return $this->accountNonExpired;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAccountNonLocked()
+    {
+        return $this->accountNonLocked;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCredentialsNonExpired()
+    {
+        return $this->credentialsNonExpired;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eraseCredentials()
+    {
+    }
   
 }
--- Security/User/BasicUserProvider.php
+++ Security/User/BasicUserProvider.php
@@ -8,13 +8,104 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\InMemoryUserProvider;
 
-class BasicUserProvider extends InMemoryUserProvider {
+class BasicUserProvider implements UserProviderInterface {
+  
+  /**
+   * @var SessionInterface
+   */
+  private $session;
+  
+  protected $users;
+  
+  protected $userDetails;
+  
+  /**
+   * @var Client
+   */
+  private $apiClient;
+
+  
+  public function __construct(\Guzzle\Service\Client $apiClient, \Symfony\Component\HttpFoundation\Session\SessionInterface $session,  array $userDetails = array()) {
     
-    /**
+    $this->apiClient   = $apiClient;
+    $this->session     = $session;
+    $this->userDetails = $userDetails;
+    
+//    foreach ($users as $username => $attributes) {
+//      $password = isset($attributes['password']) ? $attributes['password'] : null;
+//      $enabled = isset($attributes['enabled']) ? $attributes['enabled'] : true;
+//      $name     = isset($attributes['name']) ? $attributes['name'] : null;
+//      $roles = isset($attributes['roles']) ? $attributes['roles'] : array();
+//      $user = new BasicUser($username, $password, $name, $roles, $enabled, true, true, true);
+////var_dump($user);
+//      $this->createUser($user);
+//    }
+  }
+  
+  public function createUser($username, array $userDetails) {
+      if (isset($this->users[strtolower($username)])) {
+          throw new \LogicException('Another user with the same username already exists.');
+      }
+
+//      // Retrieve this user's port
+//      $portCommand  = $this->apiClient->getCommand('GetPortByName', array('name' => $userDetails['port']));
+//      $port         = $portCommand->execute();
+//      $portResponse = $port->getResponse();
+//      if (!is_object($port)) {
+//        
+//      }
+      
+      $user = new BasicUser(
+        $username,
+        $userDetails['password'],
+        $userDetails['name'],
+        $userDetails['roles'],
+        true,
+        true,
+        true,
+        true
+      );
+      
+      return $user;
+  }
+  
+  /**
+    * {@inheritdoc}
+    */
+   public function loadUserByUsername($username)
+   {
+      if (!isset($this->userDetails[strtolower($username)])) {
+          $ex = new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+          $ex->setUsername($username);
+
+          throw $ex;
+      }
+
+//      $user = $this->users[strtolower($username)];
+
+      $user = $this->createUser($username, $this->userDetails[strtolower($username)]);
+      
+      return new BasicUser($user->getUsername(), $user->getPassword(), $user->getName(), $user->getRoles(), $user->isEnabled(), $user->isAccountNonExpired(),
+              $user->isCredentialsNonExpired(), $user->isAccountNonLocked());
+   }
+   
+   /**
      * {@inheritDoc}
      */
-    public function supportsClass($class) {
-        return $class === 'BorderForce\Drt\EntityBundle\Security\User\User';
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
     }
+  
+  /**
+   * {@inheritDoc}
+   */
+  public function supportsClass($class) {
+      return $class === 'BorderForce\Drt\EntityBundle\Security\User\BasicUser';
+  }
 }
 
